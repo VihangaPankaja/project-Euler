63aaf981059ed51711051a5231b2e76296309423
VihangaPankaja <73072737+VihangaPankaja@users.noreply.github.com>
2021-05-18 20:40:46 +0530

    .

---
+++ test39-Integer right triangles.py
@@ -0,0 +1,11 @@
+# If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+# {20,48,52}, {24,45,51}, {30,40,50}
+# For which value of p ≤ 1000, is the number of solutions maximised?
+
+# a,b,c    a**2+b**2 = c**2    
+# m>n>0 in Z   k in Z
+# a = k(m**2 - n**2)
+# b = k(2mn)
+# c = k(m**2 + n**2)
+
+
