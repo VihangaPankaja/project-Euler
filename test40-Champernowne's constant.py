00f6f914571b148e2ae87c686c68c6ab597b02b5
VihangaPankaja <73072737+VihangaPankaja@users.noreply.github.com>
2021-05-20 18:07:49 +0530

    40 done

--- test40-Champernowne's constant.py
+++ test40-Champernowne's constant.py
@@ -11,8 +11,32 @@
 
 
 def d_n(n: int)-> int:
+    if n <= 0:
+        return None
     
-    pass
+    elif 1 <= n <= 9:
+        return n
+    
+    else:
+        last_digit = 9
+        power = 1       # 10 th power (digits in decimal number)
+
+        while True:
+            if (last_digit + 1) <= n <= (last_digit + (power + 1) * (10**(power + 1) - 10**power)):    # digits in decimal number
+                
+                n -= last_digit    # remove nth digits till previous digit
+
+                num = (10**power -1) + ( (n//(power + 1) + 1) if n%(power + 1)!=0 else (n//(power + 1)) )   # selct number in nth digit
+                ######{last num of   ###{ quotient + 1}#########{remainder is 0 }#######{   quotient   }####
+                ######previous digit}#######################################################################
+                ## eg: 10 -> 10-9=1 -> quo=0 rem=1
+                ##     11 -> 11-9=2 -> quo=1 rem=0
+                
+                return int(str(num)[n%(power + 1) - 1])   # return requied digit from selected number 
+            
+            else:
+                last_digit += (power + 1) * (10**(power + 1) - 10**power)   # concatinated digits for current number of digitss
+                power += 1      # next 10th power
     
 
 
