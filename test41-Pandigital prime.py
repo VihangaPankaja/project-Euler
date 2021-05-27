d145af298f28b5f4aa9034c6560a9272c2ab5c87
VihangaPankaja <73072737+VihangaPankaja@users.noreply.github.com>
2021-05-27 21:06:26 +0530

    41 done and started 42

--- test41-Pandigital prime.py
+++ test41-Pandigital prime.py
@@ -2,3 +2,16 @@
 
 # What is the largest n-digit pandigital prime that exists?
 
+from itertools import permutations
+from my_math import is_prime
+
+
+def largest_pandigital_prime()-> int:
+    for len_digits in range(9,0,-1):        # number of digits in pandigital number. starting from highest to reduce run time
+        for digit in permutations(map(str, range(len_digits,0,-1))):        # generate permutation of given digits starting from highest to reduce run time
+            if is_prime(int(''.join(digit))):       # convert digits to number and check for primality
+                return int(''.join(digit))
+
+
+if __name__ == '__main__':
+    print(largest_pandigital_prime())
