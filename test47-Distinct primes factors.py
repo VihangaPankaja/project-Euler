368811ae60e47e59b4176f056401a8a1bb301fb2
VihangaPankaja <73072737+VihangaPankaja@users.noreply.github.com>
2021-06-04 21:22:44 +0530

    47 start

---
+++ test47-Distinct primes factors.py
@@ -0,0 +1,27 @@
+""" 
+  The first two consecutive numbers to have two distinct prime factors are:
+*        14 = 2 × 7
+*        15 = 3 × 5
+
+  The first three consecutive numbers to have three distinct prime factors are:
+*        644 = 2² × 7  × 23
+*        645 = 3  × 5  × 43
+*        646 = 2  × 17 × 19.
+
+? Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers
+"""
+
+def distinct_prime_factors(n: int)-> int:
+    """ reurns how many distinct prime factors to given number """
+
+    pass
+    
+    
+def consecutive_prime_factors(distinct_factors: int)-> int:
+    """ returns first number for first n consecutive integers to have distinct n prime factors """
+    
+    pass
+
+
+if __name__ == "__main__":
+    print(consecutive_prime_factors(4))
