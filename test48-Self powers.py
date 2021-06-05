6cdb973bf4780603e39b1d399b15564fe534e4f4
VihangaPankaja <73072737+VihangaPankaja@users.noreply.github.com>
2021-06-05 22:38:40 +0530

    48 done in python

---
+++ test48-Self powers.py
@@ -0,0 +1,14 @@
+""" 
+  The series, 
+*        1¹ + 2² + 3³ + ... + 10¹⁰ = 10405071317.
+
+? Find the last ten digits of the series, 1¹ + 2² + 3³ + ... + 1000¹⁰⁰⁰.
+"""
+
+
+def main(terms: int, last_digit: int) -> str:
+    return str(sum([n**n for n in range(1, terms+1)]))[-last_digit:]
+
+
+if __name__ == '__main__':
+    print(main(1_000, 10))
