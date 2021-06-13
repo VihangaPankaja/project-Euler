e66829fdb9954e7420d607aee55f9ef6eef8089e
VihangaPankaja <73072737+VihangaPankaja@users.noreply.github.com>
2021-06-13 15:08:24 +0530

    optimized 43

--- test43-Sub-string divisibility.py
+++ test43-Sub-string divisibility.py
@@ -10,16 +10,16 @@
 *       𝑑₅𝑑₆𝑑₇  = 357   is divisible by 7
 *       𝑑₆𝑑₇𝑑₈  = 572   is divisible by 11
 *       𝑑₇𝑑₈𝑑₉  = 728   is divisible by 13
-*       𝑑₇𝑑₉𝑑₁₀ = 289   is divisible by 17
+*       𝑑₈𝑑₉𝑑₁₀ = 289   is divisible by 17
 
 ? Find the sum of all 0 to 9 pandigital numbers with this property.
 """
 
-from typing import Generator, Iterator
+from typing import Generator
 from itertools import permutations
 
 
-def divicible_pandigital(divicible: dict[str, Iterator[str]]) -> Generator[int, None, None]:
+def divicible_pandigital(divicible: dict[str, dict[str, bool]]) -> Generator[int, None, None]:
     """ 
         generates 0 to 9 digits pandigital numbers that are follows
             * 𝑑₂𝑑₃𝑑₄  is divisible by 2
@@ -31,35 +31,54 @@ def divicible_pandigital(divicible: dict[str, Iterator[str]]) -> Generator[int, 
             * 𝑑₈𝑑₉𝑑₁₀ is divisible by 17
         rules where 𝑑ₙ is 𝑛ᵗʰ digit
     """
-    
+
     for digits in permutations(map(str, range(10))):
         num: str = ''.join(digits)   # digits to number
-        
+
         if all((
-                num[0]!='0', 
-                num[1:4] in divicible['d2'], 
-                num[2:5] in divicible['d3'], 
-                num[3:6] in divicible['d5'], 
-                num[4:7] in divicible['d7'], 
-                num[5:8] in divicible['d11'], 
-                num[6:9] in divicible['d13'], 
-                num[7:10] in divicible['d17']
-            )):     # check if follow digits divicible rules follows and not starts with 0 
+            num[0] != '0',
+            divicible['d2'][num[1:4]],
+            divicible['d3'][num[2:5]],
+            divicible['d5'][num[3:6]],
+            divicible['d7'][num[4:7]],
+            divicible['d11'][num[5:8]],
+            divicible['d13'][num[6:9]],
+            divicible['d17'][num[7:10]]
+        )):     # check if follow digits divicible rules follows and not starts with 0
             yield int(num)
 
 
-if __name__ == "__main__":
-    #################################################################################################################################################
-    divicible: dict[str, list[str]] = {
-        ###############    filter only has different digits     ############ fill 0 till 3 digits ## numbers divicible by 2 have max 3 digits ###
-        'd2':list(filter(lambda x: all([x.count(i)==1 for i in x]), map( lambda x:str(x).rjust(3,'0') , [ i for i in range(1000) if i%2 == 0 ] ))),
-        'd3':list(filter(lambda x: all([x.count(i)==1 for i in x]), map( lambda x:str(x).rjust(3,'0') , [ i for i in range(1000) if i%3 == 0 ] ))),
-        'd5':list(filter(lambda x: all([x.count(i)==1 for i in x]), map( lambda x:str(x).rjust(3,'0') , [ i for i in range(1000) if i%5 == 0 ] ))),
-        'd7':list(filter(lambda x: all([x.count(i)==1 for i in x]), map( lambda x:str(x).rjust(3,'0') , [ i for i in range(1000) if i%7 == 0 ] ))),
-        'd11':list(filter(lambda x: all([x.count(i)==1 for i in x]), map( lambda x:str(x).rjust(3,'0') , [ i for i in range(1000) if i%11 == 0 ] ))),
-        'd13':list(filter(lambda x: all([x.count(i)==1 for i in x]), map( lambda x:str(x).rjust(3,'0') , [ i for i in range(1000) if i%13 == 0 ] ))),
-        'd17':list(filter(lambda x: all([x.count(i)==1 for i in x]), map( lambda x:str(x).rjust(3,'0') , [ i for i in range(1000) if i%17 == 0 ] )))
+def make_divisible_dic():
+    nums = [k for i in range(1000) if len(set((k:= str(i).rjust(3, '0')))) == 3]
+
+    divicible: dict[str, dict[str, bool]] = {
+        'd2': {i: False for i in nums},
+        'd3': {i: False for i in nums},
+        'd5': {i: False for i in nums},
+        'd7': {i: False for i in nums},
+        'd11': {i: False for i in nums},
+        'd13': {i: False for i in nums},
+        'd17': {i: False for i in nums}
     }
-    ##################################################################################################################################################
-    
-    print(sum(divicible_pandigital(divicible)))
+
+    for i in divicible['d2'].keys():
+        if int(i) % 2 == 0:
+            divicible['d2'][i] = True
+        if int(i) % 3 == 0:
+            divicible['d3'][i] = True
+        if int(i) % 5 == 0:
+            divicible['d5'][i] = True
+        if int(i) % 7 == 0:
+            divicible['d7'][i] = True
+        if int(i) % 11 == 0:
+            divicible['d11'][i] = True
+        if int(i) % 13 == 0:
+            divicible['d13'][i] = True
+        if int(i) % 17 == 0:
+            divicible['d17'][i] = True
+            
+    return divicible
+
+
+if __name__ == "__main__":
+    print(sum(divicible_pandigital(make_divisible_dic())))
