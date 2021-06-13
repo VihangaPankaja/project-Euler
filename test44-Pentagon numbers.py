94530d6c8241a15d7eda50305d55d6e5a574f431
VihangaPankaja <73072737+VihangaPankaja@users.noreply.github.com>
2021-06-13 15:13:26 +0530

    optimized 44

--- test44-Pentagon numbers.py
+++ test44-Pentagon numbers.py
@@ -39,11 +39,9 @@ def is_pentagonal(n: int) -> bool:
 def main() -> int:
     """ find the pair of pentagonal numbers that sum and difference also penagonal and 𝐷 is minimised """
 
-    D_min: int = None
-    
     for index_k, P_k in enumerate(pentagonal()):    # 𝑃ₖ values
 
-        start: int = int((1 + (72*index_k + 49)**0.5) /6) if D_min is None else int((1 + (24*D_min + 1)**0.5) /6)   
+        start: int = int((1 + (72*index_k + 49)**0.5) /6) 
         # reduce unnecessary computation by strat from higher term
         
         for P_j in pentagonal(start):   # 𝑃ⱼ values
@@ -53,20 +51,7 @@ def main() -> int:
             if (is_pentagonal(P_k - P_j) and 
                 is_pentagonal(P_k + P_j)):   # addtion and substractiona aloso in pentagonal
                 
-                if (D_min is None or 
-                    D_min > P_k - P_j):
-                    
-                    D_min = P_k - P_j
-                    
-                print(P_k - P_j)            # first 𝐷 is answer but after find may take long time to reach loop end
-                
-        if (D_min is not None and 
-            D_min <= 3*index_k + 2):    # when 𝑃ₖ - 𝑃₍ₖ₋₁₎ > 𝐷ₘᵢₙ , more smallar 𝐷 is not possible
-            
-            break
-        
-    return D_min
-    
+                return P_k - P_j           # first 𝐷 is answer but after find may take long time to reach loop end
 
 if __name__ == '__main__':
     print(main())
